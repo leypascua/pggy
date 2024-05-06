7a81d19aae7f408cb19cf471d5596a1b800f2ff7
Harley Pascua <pascua@greenova.de>
2024-05-07 01:11:10 +0200

    enhance ts humanize

--- src/Pggy.Cli/Infrastructure/Helpers.cs
+++ src/Pggy.Cli/Infrastructure/Helpers.cs
@@ -10,11 +10,22 @@ namespace Pggy.Cli.Infrastructure
     {
         public static string Humanize(this TimeSpan ts)
         {
-            string result = ts.TotalMilliseconds < 999 ?
-                $"{ts.TotalMilliseconds} ms" :
-                string.Format("{0:%h} hours {0:%m} minutes {0:%s} seconds", ts);
+            if (ts.TotalSeconds < 1)
+            {
+                return $"{ts.TotalMilliseconds} ms";
+            }
 
-            return result;
+            if (ts.TotalSeconds < 120)
+            {
+                return $"{ts.TotalSeconds} seconds";
+            }
+
+            if (ts.TotalMinutes < 120)
+            {
+                return $"{ts.TotalMinutes} minutes";
+            }
+
+            return string.Format("{0:%h} hours {0:%m} minutes {0:%s} seconds", ts);
         }
     }
 }
